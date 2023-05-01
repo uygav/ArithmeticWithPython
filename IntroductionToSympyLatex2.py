1e30bd6528a2785c64ca907d2aad60391a12ce22
Uygar Muntaş <muntasuygar@gmail.com>
2023-05-01 21:26:23 +0300

    intro to latex

---
+++ IntroductionToSympyLatex2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from IPython.display import Math,display
+
+display("4 +3 = 7")
+display(Math('4 + 3 = 7'))
+
+
+# In[6]:
+
+
+display(Math('\\sigma = \\mu \\times \\sqrt{5}'))
+
+
+# In[10]:
+
+
+display(Math('x_m'))
+display(Math('x_mm'))
+display(Math('x_{mm}'))
+display(Math('y^nm'))
+display(Math('y^{nm}'))
+
+
+# In[12]:
+
+
+display(Math('\\sqrt{x} \\sqrt{x}'))
+
+
+# In[15]:
+
+
+display(Math('\\text {the answer to this equation is :} \\frac{1+x}{2uygar}'))
+
+
+# # this is a markdown cell
+# this cell is not for runnning actual Python code . we can make the following math expression:
+# $$\\frac{1+x}{2uygar}$$
+# $$\frac{1+x}{2uygar}$$
+
+# In[ ]:
+
+
+
+
